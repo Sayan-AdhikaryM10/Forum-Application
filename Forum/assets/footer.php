53cb47a63ed2143e639a2e0a21cd6434eee98682
Sayan Adhikary <147737664+Sayan-AdhikaryM10@users.noreply.github.com>
2025-06-24 21:50:55 +0530

    Delete Forum directory

--- Forum/assets/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="container-fluid bg-dark">
-    <p class="text-center text-light my-0 py-3">CopyRights reserved 2025 | Made By Sayan Adhikary</p>
-</div>
--- Forum/assets/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("location: /Forum/index.php?logout=true");
-exit();
-?>
